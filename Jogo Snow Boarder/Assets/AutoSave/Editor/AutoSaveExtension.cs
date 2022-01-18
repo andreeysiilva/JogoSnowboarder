75d5ab15858813cd792b9328cffde5fc7d67de50
Andrey Silva <82220976+andreeysiilva@users.noreply.github.com>
2022-01-18 00:05:29 -0300

    Adicionado Neve Assets

---
+++ Jogo Snow Boarder/Assets/AutoSave/Editor/AutoSaveExtension.cs
@@ -0,0 +1,16 @@
+﻿using UnityEditor;
+using UnityEditor.SceneManagement;
+
+[InitializeOnLoad]
+public class AutoSaveExtension {
+    static AutoSaveExtension() {
+        EditorApplication.playModeStateChanged += AutoSaveWhenPlaymodeStarts;
+    }
+	
+    private static void AutoSaveWhenPlaymodeStarts(PlayModeStateChange state) {
+        if (EditorApplication.isPlayingOrWillChangePlaymode && !EditorApplication.isPlaying) {
+            EditorSceneManager.SaveScene(EditorSceneManager.GetActiveScene(), null, false);
+            AssetDatabase.SaveAssets();
+        }
+    }
+}

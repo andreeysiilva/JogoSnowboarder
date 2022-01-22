564e72f0cf454c0c1222a01bd0623777f312a32f
Andrey Silva <82220976+andreeysiilva@users.noreply.github.com>
2022-01-22 00:42:35 -0300

    FInalizado Entrada e Saida de Particulas do Snowboard

--- SnowBoarder/Assets/Scripts/CrashDetector.cs
+++ SnowBoarder/Assets/Scripts/CrashDetector.cs
@@ -1,14 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class CrashDetector : MonoBehaviour
 {
+    [SerializeField] float delayScene = 0.5f;
+    [SerializeField] ParticleSystem crashEffect;
     void OnTriggerEnter2D(Collider2D other)
     {
         if (other.tag == "Ground")
         {
-            Debug.Log("Cabeça");
+            crashEffect.Play();
+            Invoke("ReloadScene", delayScene);
         }
+    }
+
+    void ReloadScene()
+    {
+        SceneManager.LoadScene(0);
     }
 }
--- SnowBoarder/Assets/Scripts/FinishLine.cs
+++ SnowBoarder/Assets/Scripts/FinishLine.cs
@@ -1,14 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class FinishLine : MonoBehaviour
 {
+    [SerializeField] float delayScene = 1f;
+    [SerializeField] ParticleSystem finishEffect;
     void OnTriggerEnter2D(Collider2D other)
     {
         if (other.tag == "Player")
         {
-            Debug.Log("Teste1");
+            finishEffect.Play();
+            Invoke("ReloadScene", delayScene);
         }
+    }
+
+    void ReloadScene()
+    {
+        SceneManager.LoadScene(0);
     }
 }
